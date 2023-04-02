877ffa576f9b9c30b5a2023515602ce2c639befd
Prabal Pathak <prabal01pathak@gmail.com>
2023-04-02 14:48:44 +0530

    initial commit

---
+++ header.php
@@ -0,0 +1,46 @@
+<?php 
+session_start();
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+	// echo "<h1>Hi, <b>" . htmlspecialchars($_SESSION["username"]) . "</b>. Welcome.</h1>";
+
+} else {
+	header("location: login.php");
+	exit;
+}
+?>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css/header.css">
+</head>
+<header>
+<div class="header">
+	<div class="logo">
+		<a href="index.php">Biometric Attendance</a>
+	</div>
+</div>
+
+<div class="topnav" id="myTopnav">
+	<a href="index.php">Users</a>
+    <a href="UsersLog.php">Users Log</a>
+    <a href="ManageUsers.php">Manage Users</a>
+    <a href="logout.php">Logout</a>
+    <a href="javascript:void(0);" class="icon" onclick="navFunction()">
+	  <i class="fa fa-bars"></i></a>
+</div>
+</header>
+<script>
+	function navFunction() {
+	  var x = document.getElementById("myTopnav");
+	  if (x.className === "topnav") {
+	    x.className += " responsive";
+	  } else {
+	    x.className = "topnav";
+	  }
+	}
+</script>
+
+
+	
+
+	
